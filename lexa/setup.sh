a9445826459673c96a24bb110604765bf86a628e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-09-23 12:43:50 -0400

    added shortcut shell scripts

---
+++ lexa/setup.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=/home/ei-lab/rbhagat8/lexa/lexa:/home/ei-lab/rbhagat8/lexa-benchmark
+export MUJOCO_RENDERER=egl; export MUJOCO_GL=egl
+export MJLIB_PATH=~/.mujoco/mujoco200/bin/libmujoco200.so
+export MJKEY_PATH=~/.mujoco/mjkey.txt
+conda activate lexa_env
---
+++ lexa/train.sh
@@ -0,0 +1,1 @@
+python train.py --configs defaults lexa_temporal --task dmc_walker_walk --logdir ~/logdir/lexa_temporal_walker
